340664c910b5bbec8058219a34545c2b63f73659
kanchanshrestha11 <kanchanshrestha790@gmail.com>
2026-08-24 07:48:37 +0545

    Remove login page

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,17 @@
-import { Navigate, Route, Routes, useLocation } from "react-router-dom";
+import { Navigate, Route, Routes } from "react-router-dom";
 import Navbar from "./Components/Navbar";
 import Home from "./Pages/Home";
 import About from "./Pages/About";
-import Login from "./Pages/Login";
 import Counter from "./Counter";
 import Favorites from "./Pages/Favorites";
 import "./App.css";
 
 function App() {
-  const { pathname } = useLocation();
-  const isLoginPage = pathname === "/" || pathname === "/login";
-
   return (
     <div className="App">
-      {!isLoginPage && <Navbar />}
+      <Navbar />
       <Routes>
-        <Route path="/" element={<Login />} />
-        <Route path="/login" element={<Login />} />
+        <Route path="/" element={<Home />} />
         <Route path="/home" element={<Home />} />
         <Route path="/About" element={<About />} />
         <Route path="/counter" element={<Counter />} />
